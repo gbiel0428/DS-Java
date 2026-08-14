bb9cf233c957e98b113f88fb58f50073b56eea85
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-14 15:18:35 -0300

    salvando

--- AULA - 1/Atividade2.java
+++ AULA - 1/Atividade2.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Atividade2 {
     public static void main(String[] args) {
-        String nome="Gabriel";
-        int nota1 = 10;
-        int nota2 = 8;
-        double media = (nota1 + nota2) / 2.0;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.printf("Aluno: %s | Nota 1: %d | Nota 2: %d | Media do Aluno: %.2f",nome,nota1,nota2,media);
+        System.out.print( "Informe o nome do Aluno:");
+        String nome = sc.next();
+
+        System.out.print("Informe a 1- NOTA DO ALUNO:");
+        int nota1 = sc.nextInt();
+
+        System.out.print("Informe a 2- NOTA DO ALUNO:");
+        int nota2= sc.nextInt();
+
+        double media= (nota1 + nota2) / 2;
+
+        System.out.printf("Aluno: %s  | 1-NOTA: %d   | 2-NOTA: %d  | Media: %.2f",nome,nota1,nota2,media);
+        sc.close();
     }
 }
